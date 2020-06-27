e2bea38708c6ab98532f4675955c5bee64473747
Laura R. Marusich <laura.m.cooper20.civ@mail.mil>
2020-06-27 23:00:02 +0000

    version 0.4.0

--- R/rmcorr.R
+++ R/rmcorr.R
@@ -4,6 +4,7 @@
 #' @param measure1 A numeric variable giving the observations for one measure.
 #' @param measure2 A numeric variable giving the observations for the second measure.
 #' @param dataset The data frame containing the variables.
+#' @param CI.level The confidence level of the interval
 #' @param CIs The method of calculating confidence intervals.
 #' @param nreps The number of resamples to take if bootstrapping.
 #' @param bstrap.out Determines if the output include the bootstrap resamples.
@@ -14,29 +15,40 @@
 #' \item{CI}{the 95\% confidence interval for the repeated measures correlation coefficient.}
 #' \item{model}{the multiple regression model used to calculate the correlation coefficient.}
 #' \item{resamples}{the bootstrap resampled correlation values.}
+#' @references Bakdash, J.Z., & Marusich, L.R. (2017). 
+#' Repeated Measures Correlation. \emph{Frontiers in Psychology, 8}, 256. 
+#' \url{https://doi.org/10.3389/fpsyg.2017.00456}.
+#' 
+#' Bland, J.M., & Altman, D.G. (1995). Calculating correlation 
+#' coefficients with repeated observations: Part 1 - correlation within
+#' subjects. \emph{BMJ, 310}, 446.
 #' @seealso \code{\link{plot.rmc}}
 #' @examples
 #' ## Bland Altman 1995 data
 #' rmcorr(Subject, PacO2, pH, bland1995)
 #' @export
 
-rmcorr <- function(participant, measure1, measure2, dataset, 
+rmcorr <- function(participant, measure1, measure2, dataset,
+                   CI.level = 0.95,
                    CIs = c("analytic", "bootstrap"), 
                    nreps = 100, bstrap.out = F) {
+    
+    op <- options(contrasts = getOption("contrasts"))
+    on.exit(options(op))
     
     options(contrasts = c("contr.sum", "contr.poly"))
     
     args <- as.list(match.call())
     
-    Participant <- eval(args$participant, dataset)
+    Participant <- eval(args$participant, dataset, parent.frame())
     if (class(Participant) == "character"){
         Participant <- get(Participant, dataset)
     }
-    Measure1 <- eval(args$measure1, dataset)
+    Measure1 <- eval(args$measure1, dataset, parent.frame())
     if (class(Measure1) == "character"){
         Measure1 <- get(Measure1, dataset)
     }
-    Measure2 <- eval(args$measure2, dataset)
+    Measure2 <- eval(args$measure2, dataset, parent.frame())
     if (class(Measure2) == "character"){
         Measure2 <- get(Measure2, dataset)
     }
@@ -59,6 +71,10 @@ rmcorr <- function(participant, measure1, measure2, dataset,
     
     CIs <- match.arg(CIs)
     
+    if (!is.numeric(CI.level) || CI.level <= 0 || CI.level >= 1){
+        stop("'CI.level' must be a numeric value between 0 and 1")
+    }
+    
     lmmodel <- stats::lm(Measure2 ~ Participant + Measure1)
     lmslope <- stats::coef(lmmodel)["Measure1"]
     errordf <- lmmodel$df.residual
@@ -80,7 +96,7 @@ rmcorr <- function(participant, measure1, measure2, dataset,
     #analytic
     resamples <- NULL
     if (CIs == "analytic"){
-        rmcorrvalueCI <- psych::r.con(rmcorrvalue, errordf) 
+        rmcorrvalueCI <- psych::r.con(rmcorrvalue, errordf, p = CI.level) 
     } else if (CIs == "bootstrap") {
         nsubs <- length(levels(Participant))
         if (!is.numeric(nreps)){stop("Specify the number of bootstrap resamples to take")}
@@ -109,14 +125,16 @@ rmcorr <- function(participant, measure1, measure2, dataset,
             
             cor.reps[i] <- as.numeric(repsign*sqrt(SSFactor/(SSFactor+SSresidual)))
         }
-        rmcorrvalueCI <- stats::quantile(cor.reps,probs=c(.025,.975))
+        CI.limits <- c((1-CI.level)/2, (1-CI.level) + CI.level)
+        rmcorrvalueCI <- stats::quantile(cor.reps,probs=CI.limits)
         resamples <- cor.reps
     }
     
     
-    
     rmoutput <- list(r = rmcorrvalue, df = errordf, p = pvalue, 
-                     CI = rmcorrvalueCI, model = lmmodel, 
+                     CI = rmcorrvalueCI, 
+                     CI.level = CI.level,
+                     model = lmmodel, 
                      vars = as.character(c(args$participant,args$measure1,args$measure2)))
     if (bstrap.out) {rmoutput$resamples <- resamples}
     class(rmoutput) <- "rmc"
@@ -144,9 +162,8 @@ print.rmc <- function(x, ...) {
     cat(x$df)
     cat("\n\np-value\n")
     cat(x$p)
-    cat("\n\n95% confidence interval\n")
-    cat(x$CI)
+    cat("\n\n", x$CI.level*100, "% confidence interval\n", sep = "")
+    cat(x$CI,"\n\n")
     
 }   
-    
-    
+
